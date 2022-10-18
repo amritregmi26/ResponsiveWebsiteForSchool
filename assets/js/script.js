2e08a3b9f731077354f885357a0fccc54526489e
= <=>
2022-10-18 22:00:45 +0545

    minor changes made

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function redirection()
 {
-    window.location.href = 'https://amritregmi.com.np#courses/view/course.html';
+    window.location.href = 'https://amritregmi.com.np/view/course.html';
 }
 var button = document.querySelector('#courses .course-card  .card-body .btn');
 button.addEventListener('click', redirection);
