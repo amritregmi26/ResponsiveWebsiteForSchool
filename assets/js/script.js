d379226ea22899028deec116d5890784da26927d
= <=>
2022-10-18 21:34:32 +0545

    javascript added

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function redirection()
+{
+    window.location.href = 'https://amritregmi.com.np#courses/view/course.html';
+}
+var button = document.querySelector('#courses .course-card  .card-body .btn');
+button.addEventListener('click', redirection);
+
