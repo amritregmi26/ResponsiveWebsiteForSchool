977c62db78765d852175e68e0f35360ae51207a5
= <=>
2022-10-20 09:51:20 +0545

    functionality added using javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,28 @@
             window.location.href = '../about.html';
         });
     }
+    
+    function courseSectionRedirection()
+    {
+        let courseRead = document.querySelectorAll('#courses .card-body .btn');
+        for(let i = 0; i < 4; i++)
+        {
+            courseRead[i].addEventListener('click', () => {
+                window.location.href = '../course.html';
+            });
+        }
+    }
+
+    function notesPage() 
+    {
+        let submitBtn = document.querySelector('.notes-main input[type="button"');
+        submitBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+        })
+    }
 
     aboutPageRedirection();
+    courseSectionRedirection();
+    notesPage();
+
 }) (document);
