fe5a26d06163afdceb3724dcdd1953b22cf4cdf1
amritregmi26 <amrregmi@gmail.com>
2023-10-01 20:53:11 +0545

    Navigation fixed and project is 98% completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,51 @@
 !(function(d){
-    function aboutPageRedirection()
+
+    if(window.location.href == 'http://127.0.0.1:5500' || window.location.href == 'http://127.0.0.1:5500/index.html')
     {
-        let introRead = document.querySelector('.introduction .btn');
-        introRead.addEventListener('click', () => {
-            window.location.href = '../about.html';
-        });
-    }
-    
-    function courseSectionRedirection()
-    {
-        let courseRead = document.querySelectorAll('#courses .card-body .btn');
-        for(let i = 0; i < 4; i++)
+        function aboutPageRedirection()
         {
-            courseRead[i].addEventListener('click', () => {
-                window.location.href = '../course.html';
+            let introRead = d.querySelector('.introduction .btn');
+            introRead.addEventListener('click', () => {
+                window.location.href = '../about.html';
             });
         }
+
+        function courseSectionRedirection()
+        {
+            let readMoreBtn = d.querySelectorAll('#courses .card-body .btn');
+            for(let i = 0; i < 4; i++)
+            {
+                readMoreBtn[i].addEventListener('click', () => {
+                    window.location.href = '../course.html';
+                    let title = readMoreBtn[i].parentElement.children[0].children[0].innerHTML;
+                    localStorage.setItem("title", `${title}`);
+                });
+            }
+        }
+
+        // Function Call
+        courseSectionRedirection();
+        aboutPageRedirection();
     }
 
-    function notesPage() 
+    if(window.location.href == 'http://127.0.0.1:5500/notes.html')
     {
-        let submitBtn = document.querySelector('.notes-main input[type="button"');
-        submitBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-        })
+        function notesPage() 
+        {
+            let submitBtn = d.querySelector('.notes-main input[type="button"]');
+            submitBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+            })
+        }
+
+        // Function call
+        notesPage();
     }
 
-    aboutPageRedirection();
-    courseSectionRedirection();
-    notesPage();
+    if(window.location.href == 'http://127.0.0.1:5500/course.html')
+    {
+       let courseTitle = localStorage.getItem("title");
+       d.querySelector(".course-main .course-description h1").innerHTML = courseTitle;   
+    }
 
 }) (document);
